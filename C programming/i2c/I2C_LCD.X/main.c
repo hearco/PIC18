584df29674ef9480243411e12ce9a6fb86534ed9
hearco <hearco.93@gmail.com>
2018-03-17 22:51:30 -0600

    update i2c LCD

--- C programming/i2c/I2C_LCD.X/main.c
+++ C programming/i2c/I2C_LCD.X/main.c
@@ -45,13 +45,16 @@
 
 #include "mcc_generated_files/mcc.h"
 
+#define SIZE_OF_MESSAGE_BYTES       18
+#define SLAVE_I2C_GENERIC_RETRY_MAX 3
+
 /*
                          Main application
  */
 void main(void)
 {
     // Initialize the device
-    //SYSTEM_Initialize();
+    SYSTEM_Initialize();
 
     // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
     // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts
@@ -81,12 +84,77 @@ void main(void)
     // Disable the Peripheral Interrupts
     //INTERRUPT_PeripheralInterruptDisable();
     
-    ANSELB = TRISB = 0;
-    LATB = 0xFF;
+    
+    // Format of the message
+    //
+    // b7  b6  b5  b4  b3  b2  b1  b0
+    // Rs  Rw  En  (1) D4  D5  D6  D7
+    //
+    unsigned char i;
+    unsigned short timeout;
+    unsigned char LCD_commands[SIZE_OF_MESSAGE_BYTES] = {0x32, // Enable high, set 4 bit mode
+                                                        // Add 5ms delay
+                                                        0x12, // Enable low
+
+                                                        0x32, // Enable high, set 4 bit mode HIGH
+                                                        // Add 5ms delay
+                                                        0x12, // Enable low
+
+                                                        0x38, // Enable high, set 4 bit mode LOW
+                                                        // Add 5ms delay
+                                                        0x18, // Enable low
+
+                                                        0x30, // Enable high, set Cursor ON HIGH
+                                                        // Add 5ms delay
+                                                        0x10, // Enable low 
+
+                                                        0x3F, // Enable high, set Cursor ON LOW
+                                                        // Add 5ms delay
+                                                        0x1F, // Enable low
+
+                                                        0x30, // Enable high, set Cursor Home HIGH
+                                                        // Add 5ms delay
+                                                        0x10, // Enable low
+
+                                                        0x32, // Enable high, set Cursor Home LOW
+                                                        // Add 5ms delay
+                                                        0x12, // Enable low
+
+                                                        0x30, // Enable high, set Clear Screen HIGH
+                                                        // Add 5ms delay
+                                                        0x10, // Enable low 
+
+                                                        0x31, // Enable high, set Clear Screen LOW
+                                                        // Add 5ms delay
+                                                        0x11 // Enable low
+                                                        };
+    
+    unsigned char data = 0xFF;
+    unsigned char * pD;
+    unsigned char writeBuffer[1];
+    I2C1_MESSAGE_STATUS status = I2C1_MESSAGE_PENDING;
+    
+    pD = LCD_commands;
+    for(i = 0; i < SIZE_OF_MESSAGE_BYTES; i++)
+    {
+        writeBuffer[0] = *(pD + i);
+        timeout = 0;
+        while((status != I2C1_MESSAGE_FAIL) && (status != I2C1_MESSAGE_COMPLETE) && (timeout < SLAVE_I2C_GENERIC_RETRY_MAX))
+        {
+            I2C1_MasterWrite(writeBuffer, 1, 0x40, &status);
+            
+            // wait for the message to be sent or status has changed.
+            while(status == I2C1_MESSAGE_PENDING);
+            
+            timeout++;
+        }
+        
+        __delay_ms(5);
+    }
 
     while (1)
     {
-        // Add your application code
+        
     }
 }
 /**
