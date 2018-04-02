fabfb591ae9752d912444a1fc11ddb762ecd68c7
hearco <hearco.93@gmail.com>
2018-04-02 00:14:25 -0500

    Added header and credits to dataTypes.h

--- C programming/LCD.X/dataTypes.h
+++ C programming/LCD.X/dataTypes.h
@@ -1,33 +1,31 @@
-/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
- * and any derivatives exclusively with Microchip products. 
+//-HEADER
+/************************************************************************
+ *
+ * Description:
+ *        This file contains the data types definitions for PIC18
  * 
- * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
- * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
- * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
- * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
- * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ * NOTES:
+ *        N/A
  *
- * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
- * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
- * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
- * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
- * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
- * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
- * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ ***********************************************************************/
+//~HEADER
+//-CREDITS
+/***********************************************************************
  *
- * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
- * TERMS. 
- */
-
-/* 
- * File:   
- * Author: 
- * Comments:
- * Revision history: 
- */
-
-// This is a guard condition so that contents of this file are not included
-// more than once.  
+ *  Name of file: dataTypes.h
+ * 
+ *        Author: Ariel Almendariz
+ *
+ *  Date created: 02 / 04 / 2018
+ *
+ * Last revision: 02 / 04 / 2018
+ *
+ *   Modified by: N/A
+ *
+ * Date modified: N/A
+ *
+ ***********************************************************************/
+//~CREDITS
 #ifndef DATATYPES_HEADER
 #define	DATATYPES_HEADER
 
@@ -42,4 +40,4 @@ typedef signed long int SInt32_T;
 typedef signed long long int SInt64_T;
 
 #endif	/* DATATYPES_HEADER */
-
+//~END OF FILE
